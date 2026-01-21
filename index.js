405556cd1a0a2fde136cb057783c598abc8bcdc9
OscarPKT1 <mruelastipkt1@outlook.com>
2026-01-20 18:09:50 -0700

    Actualizacion

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const puerto = 3000;
+//encriptar contras (npm install bcryptjs)
 const bcrypt = require('bcryptjs');
 
 app.use(express.json())
 
+//GET SIGN IN
 app.get('/post/login', (req, resp) => {
     resp.send('Hola')
 })
 
+
+//POST SIGN UP
 //Se marca como async para usar await
 app.post('/signup', async (req, resp) => {
 
@@ -17,6 +21,7 @@ app.post('/signup', async (req, resp) => {
 
     //Encripta la contra con bcrypt.
     const hashedPassword = await bcrypt.hash(password, 10);
+
     resp.send(`your name is: ${username} and your hashedPassword es: ${hashedPassword}`);
     console.log(req.body);
 })
