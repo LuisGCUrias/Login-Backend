eb271e3c04deaf1c1284868d38d693f14271f1e0
LuisGCUrias <coronadouriasluisg@gmail.com>
2026-01-22 17:42:10 -0700

    Update databaseConection.js

--- databaseConection.js
+++ databaseConection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-        host: "74.208.204.184",
-        user: "sa",
-        password: "d$AnWY5/",
-        database: "maestro",
+        host: "confidential",
+        user: "confidential",
+        password: "confidential",
+        database: "confidential",
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
